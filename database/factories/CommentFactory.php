a8a92688ee9d9003a716d990c96fbfcd7bed1dce
khatabwedaa <khatabwedaa@gmail.com>
2019-12-25 22:53:18 +0200

    fix factories bug

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,11 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        'name'     => $faker->name(),
-        'email'    => $faker->unique()->safeEmail,
-        'comment'  => $faker->sentence(),
-        'approved' => $faker->numberBetween($min = 1, $max = 1),
-        'post_id'  => $faker->numberBetween($min = 7, $max = 20),
-
+        'name' => $faker->name(),
+        'email' => $faker->unique()->safeEmail,
+        'comment' => $faker->sentence(),
+        'post_id' => factory(App\Post::class)->create(),
     ];
 });
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,12 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
-    return [
-        //
-        'title'       => $faker->sentence(),
-        'slug'        => "post-slug" . rand(1 , 1000),
-        'category_id' => $faker->numberBetween($min = 2, $max = 6),
-        'body'        => $faker->text(),
+    $title = str_replace(".", "", $faker->sentence());
+    $slug = str_replace(" ", "-", strtolower($title));
 
+    return [
+        'title' => $title,
+        'slug' => $slug,
+        'category_id' => 1,
+        'body' => $faker->text(),
     ];
 });
